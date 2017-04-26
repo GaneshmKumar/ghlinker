c6cff4642b2cc128b809e49874523e5de26d66d5
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-04-26 15:25:53 +0530

    Changed UI

--- ghlinker.js
+++ ghlinker.js
@@ -2,7 +2,7 @@
 * @Author: GaNeShKuMaRm
 * @Date:   2017-04-08 16:16:06
 * @Last Modified by:   GaNeShKuMaRm
-* @Last Modified time: 2017-04-22 15:51:38
+* @Last Modified time: 2017-04-26 15:22:31
 */
 
 'use strict';
@@ -13,29 +13,21 @@ function getUser(username) {
     });
 }
 
-function displayProfile(div, data, username) {
-    //alert(JSON.stringify(data));
+function displayProfile(data, username) {
     var url = "http://github.com/" + username;
     var avatar_url = data.avatar_url;
     var username = data.login;
     var name = data.name;
-    var bio = data.bio;
-    var blog = data.blog;
     var email = data.email;
 
-    var html = '<div class="ghlink-wrapper"><div class="ghlink-basic-wrapper"><a href="' + url + '" target="_blank"><i class="fa fa-external-link pull-right" aria-hidden="true"></i></a><br><img class="img img-circle img-responsive ghlink-avatar" src="' + avatar_url +'" alt=""><br><span class="ghlink-name">' + name + '</span><br><span class="ghlink-name ghlink-username">' + username + '</span><br></div><div class="ghlink-advanced-wrapper"><p class="ghlink-bio">' + bio + '</p><a class="ghlink-blog" href="' + blog + '" target="_blank"><span class="fa fa-globe"></span></a><a class="ghlink-mail"  href="' + email + '" target="_blank"><span class="fa fa-envelope"> </span></a></div></div>';
-
-    $(div).append(html);
-    /*username = '#' + username;
-    $(username + ' .ghlink-name').text(data.name);
-    $(username + ' .ghlink-username').text(data.login);*/
+    var html = '<div id="' + username + '" class="col-md-3 gh-user"><img class="img img-responsive gh-img" src="' + avatar_url + '" alt=""><div class="gh-nav"><span class="gh-username">' + name + '</span><a href="mailto:' + email + '"><span><i class="fa fa-envelope"></i></span></a><a href="' + url +'" target="_blank"><span><i class="fa fa-external-link-square"></i></span></a></div></div>';
+    $('#gh-main').append(html);
 }
 
-$.fn.link = function(username) {
+$.link = function(username) {
     var promise = getUser(username);
-    var div = this;
     promise.success(function(data) {
-        displayProfile(div, data, username);
+        displayProfile(data, username);
     });
     promise.error(function(data) {
         alert(JSON.stringify(data));
--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 * @Author: GaNeShKuMaRm
 * @Date:   2017-04-08 16:19:29
 * @Last Modified by:   GaNeShKuMaRm
-* @Last Modified time: 2017-04-22 15:52:20
+* @Last Modified time: 2017-04-26 15:16:56
 */
 
 'use strict';
-$("#ganesh").link("dhanushuuzumaki");
+$.link("ganeshmkumar");
+$.link("dhanushuuzumaki");
+$.link("selvajove");
+$.link("suren99");
